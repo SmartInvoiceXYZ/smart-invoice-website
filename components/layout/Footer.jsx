0194521dd1f2dccaaa49221fb442b7c583d28086
geovgy <gbh218@gmail.com>
2023-08-07 15:40:30 -0400

    Add discord invite to footer

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -154,6 +154,9 @@ export function Footer({ ...props }) {
           <NextLink href="https://twitter.com/SmartInvoiceXYZ" target="_blank" passHref>
             <Link target="_blank">Twitter</Link>
           </NextLink>
+          <NextLink href="https://discord.gg/t4n25qrUtN" target="_blank" passHref>
+            <Link target="_blank">Discord</Link>
+          </NextLink>
         </Flex>
       </Flex>
     </Flex>
