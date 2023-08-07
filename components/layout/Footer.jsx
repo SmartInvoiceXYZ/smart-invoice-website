9885a846b9529c7613232e12ec46aab76cd3fda9
geovgy <gbh218@gmail.com>
2023-08-07 15:44:06 -0400

    Changed discord invite link

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -154,7 +154,7 @@ export function Footer({ ...props }) {
           <NextLink href="https://twitter.com/SmartInvoiceXYZ" target="_blank" passHref>
             <Link target="_blank">Twitter</Link>
           </NextLink>
-          <NextLink href="https://discord.gg/t4n25qrUtN" target="_blank" passHref>
+          <NextLink href="https://discord.gg/Rws3gEu8W7" target="_blank" passHref>
             <Link target="_blank">Discord</Link>
           </NextLink>
         </Flex>
